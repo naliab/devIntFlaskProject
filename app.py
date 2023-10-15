96ec651c04a52cda8530c809ea038729fb976370
Игорь Степанов <istepanov@lanit.ru>
2023-10-15 12:51:49 +0300

    Настройка ручной миграции данных

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:12345678@localhost:3306/flask'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
--- database.py
+++ database.py
@@ -1,10 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from app import *
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://:@/'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+from app import db
 
 
 class Profile(db.Model):
